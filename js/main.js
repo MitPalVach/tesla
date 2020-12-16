18ed25f431fb6ebd4c534eb5a4b7fe6378ce6550
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2020-12-16 21:22:03 +0300

    header slider by slick

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
+    $('.slider').slick({
+        arrows: false,
+        infinite: true,
+        dots: true,
+        fade: true,
+        speed: 1000,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
 });
