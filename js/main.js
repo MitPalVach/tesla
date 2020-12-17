fb82f8e94d560ccc3f1dadefe5a05ea381a24b3d
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2020-12-17 04:11:53 +0300

    nav by jquery

--- js/main.js
+++ js/main.js
@@ -11,4 +11,14 @@ $(function(){
         slidesToScroll: 1
     });
 
+    $('.header-btn').on('click', function (){
+       $('.menu').addClass('active');
+    });
+
+    $('.close-btn').on('click', function (){
+        $('.menu').removeClass('active');
+    })
+
+
+
 });
